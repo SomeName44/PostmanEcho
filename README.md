01c0ded1446ce7e1f62dcf4ce0d01a6ed9e1e827
Olga <deputat.d@yandex.com>
2023-04-04 15:34:16 +0300

    Add test whith failed

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/yf37o8xefst19yve/branch/main?svg=true)](https://ci.appveyor.com/project/SomeName44/postmanecho/branch/main)
---
+++ src/test/java/PostmanEchoTest1.groovy
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test
+
+class PostmanEchoTest1 {
+    @Test
+    void shoudReturnPost() {
+        given()
+         .baseUri("https://postman-echo.com")
+         .contentType("text/plain; charset=UTF-8")
+         .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+         .when()
+         .post("/post")
+// Проверки
+         .then()
+         .statusCode(404)
+         .body("data", equalTo("some value"));
+    }
+}
