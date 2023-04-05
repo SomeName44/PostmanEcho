6dfbaadccd484bc30b1ab07f00cfc11fe5bbf5c2
Olga <deputat.d@yandex.com>
2023-04-05 14:28:51 +0300

    Changed test

--- src/test/java/PostmanEchoTest1.groovy
+++ src/test/java/PostmanEchoTest1.groovy
@@ -13,6 +13,6 @@ class PostmanEchoTest1 {
 // Проверки
          .then()
          .statusCode(200)
-         .body("data", equalTo("some data"));
+         .body("data", equalTo("some value"));
     }
 }
